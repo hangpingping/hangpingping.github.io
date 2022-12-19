6a0c0f5840f07b798f7a0e2686f413cdc9897400
gypsophila <hangpingH@163.com>
2022-12-19 22:01:17 +0800

    initial

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  title: "Gypsophila's Notes",
+  description: "",
+  themeConfig: {
+    nav: [
+      {
+        text: "Home",
+        link: "/",
+      },
+      {
+        text: "About",
+        ariaLabel: "About-Menu",
+        items: [
+          {
+            text: "Github",
+            link: "https://github.com/hangpingping",
+          },
+          {
+            text: "Gitee",
+            link: "https://gitee.com/hangpingh",
+          },
+        ],
+      },
+    ],
+  },
+};
